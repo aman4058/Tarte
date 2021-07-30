dc9bd2049a9b810c387504ded721aacdbd1b24a3
uspl-00267 <uspl-00267@USPL-267.unicodesystems.ad>
2021-07-30 18:22:39 +0530

    Price validation

---
+++ Tarte/src/testCases/PriceValidation.java
@@ -0,0 +1,19 @@
+package testCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PriceValidation {
+	
+public static void main (String args[]) {
+	
+   /*
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\uspl-00267\\eclipse-workspace\\Webdriver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://storefront:glamazon1375@staging.tartecosmetics.com");
+	*/
+	
+	System.out.println("Aman");
+}
+
+}
